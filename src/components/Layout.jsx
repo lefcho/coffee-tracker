c2e857f3412dede2d36e1202b2376f405bf76313
ivand <ivan.d.kostov@gmail.com>
2025-01-26 23:03:39 +0200

    added: Hero and Layout components

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,26 +1,43 @@
-import React from 'react'
+import React from "react";
 
 function Layout(props) {
+  const { children } = props;
 
-    const { children } = props;
-        
-    const header = (
-        <header></header>
-    )
+  const header = (
+    <header>
+      <div>
+        <h1 className="text-gradient">CAFFIEND</h1>
+        <p>For Coffee Insatiates</p>
+      </div>
+      <button>
+        <p>Sign up free</p>
+        <i className="fa-solid fa-mug-hot"></i>
+      </button>
+    </header>
+  );
 
-    const footer = (
-        <footer></footer>
-    )
+  const footer = (
+    <footer>
+      <p>
+        <span className="text-gradient">Caffiend</span> was made by{" "}
+        <a href="#" target="_blank">
+        Lefcho
+        </a>{" "}
+        using the{" "}
+        <a href="https://www.fantacss.smoljames.com" target="_blank">
+          FantaCSS
+        </a>
+      </p>
+    </footer>
+  );
 
-    return (
-        <>
-            {header}
-            <main>
-                {children}
-            </main>
-            {footer}
-        </>
-    )
+  return (
+    <>
+      {header}
+      <main>{children}</main>
+      {footer}
+    </>
+  );
 }
 
-export default Layout
+export default Layout;
