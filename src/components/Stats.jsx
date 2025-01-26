555dfa0f504c9b7ff312961a5580f3d4489f93c0
ivand <ivan.d.kostov@gmail.com>
2025-01-27 00:55:40 +0200

    added: start of the stats component

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
 
-function Stats() {
-  return (
-    <div>Stats</div>
-  )
+function StatCard(props) {
+    const { isLg, title, children } = props
+
+    return (
+        <div className={'card stat-card ' + (isLg ? 'col-span-2' : '')}>
+
+        </div>
+    )
 }
 
-export default Stats
+function Stats() {
+    return (
+        <>
+        <div className="section-header">
+            <i className="fa-solid fa-chart-simple" />
+            <h2>Stats</h2>
+        </div>
+        <div className="stats-grid">
+            <StatCard isLg={true}></StatCard>
+            <StatCard></StatCard>
+            <StatCard></StatCard>
+            <StatCard></StatCard>
+            <StatCard></StatCard>
+        </div>
+        </>
+    );
+}
+
+export default Stats;
