6e068ffa980c40ebc818a1b5a6e37aa5a7f426df
ivand <ivan.d.kostov@gmail.com>
2025-01-27 20:15:19 +0200

    added: history started

--- src/components/History.jsx
+++ src/components/History.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 
 function History() {
-  return (
-    <div>History</div>
-  )
+    return (
+        <>
+        <div className="section-header">
+            <i className="fa-solid fa-timeline" />
+            <h2>History</h2>
+        </div>
+        </>
+    )
 }
 
 export default History
