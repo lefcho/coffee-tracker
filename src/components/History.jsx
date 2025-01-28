d57cf878deb51d4952015b445e6cbb07530e4e48
ivand <ivan.d.kostov@gmail.com>
2025-01-28 20:57:32 +0200

    added: history component

--- src/components/History.jsx
+++ src/components/History.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { calculateCurrentCaffeineLevel, coffeeConsumptionHistory, getCaffeineAmount, timeSinceConsumption } from '../utils'
 
 function History() {
     return (
@@ -6,6 +7,29 @@ function History() {
         <div className="section-header">
             <i className="fa-solid fa-timeline" />
             <h2>History</h2>
+        </div>
+        <p><i>Hover for more information.</i></p>
+        <div className='coffee-history'>
+            {Object.keys(coffeeConsumptionHistory)
+                .sort((a, b) => b - a)
+                .map((utcTime, coffeeIndex) => {
+
+                    const coffee = coffeeConsumptionHistory[utcTime];
+                    const timeSinceConsume = timeSinceConsumption(utcTime);
+                    const originalAmount = getCaffeineAmount(coffee.name);
+                    const remainingAmount = calculateCurrentCaffeineLevel({
+                        [utcTime]: coffee
+                    })
+
+                    const summary = `${coffee.name} | ${timeSinceConsume} | $${coffee.cost}
+                    ${remainingAmount}/${originalAmount}mg`
+
+                    return (
+                        <div title={summary} key={coffeeIndex}>
+                            <i className='fa-solid fa-mug-hot'></i>
+                        </div>
+                    )
+                })}
         </div>
         </>
     )
